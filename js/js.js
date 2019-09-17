1abf9682cfa4453d11116ea555c644001057fd9e
PUGE <100284685@qq.com>
2019-09-18 01:01:58 +0800

    初步实现

--- js/js.js
+++ js/js.js
@@ -1,59 +1,72 @@
-window.onload=function(){
-    var size=[
-        {"top":60,"left":0,"width":400,"height":240,"zIndex":1},
-        {"top":60,"left":0,"width":400,"height":240,"zIndex":2},
-        {"top":30,"left":150,"width":500,"height":300,"zIndex":3},
-        {"top":0,"left":300,"width":600,"height":360,"zIndex":4},
-        {"top":30,"left":550,"width":500,"height":300,"zIndex":3},
-        {"top":60,"left":800,"width":400,"height":240,"zIndex":2},
-        {"top":60,"left":800,"width":400,"height":240,"zIndex":1}
-    ];
-    var wrap=document.getElementById('wrap');
-    var cont=wrap.firstElementChild || wrap.firstChild;
+window.onload = function() {
+   // 5个轮播
+    // var size = [
+    //   {"top":60,"left":"0%", "width":"20%","height":240,"zIndex":2},
+    //   {"top":30,"left":"15%", "width":"30%","height":300,"zIndex":3},
+    //   {"top":0,"left":"30%", "width":"40%","height":360,"zIndex":4},
+    //   {"top":30,"left":"55%", "width":"30%","height":300,"zIndex":3},
+    //   {"top":60,"left":"80%", "width":"20%","height":240,"zIndex":2},
+    //   {"top":60,"left":"40%",  "width":"20%","height":240,"zIndex":1},
+    //   {"top":60,"left":"40%",  "width":"20%","height":240,"zIndex":1},
+    // ]
+    // 3个轮播
+    var size = [
+      {"left":"0%", "width":"30%", "height":300, class:"swiper-item show"},
+      {"left":"32%", "width":"36%", "height":360, class:"swiper-item show active"},
+      {"left":"70%", "width":"30%", "height":300, class:"swiper-item show"},
+      {"left":"40%", "width":"20%", "height":240, class:"swiper-item hide"},
+      {"left":"40%", "width":"20%", "height":240, class:"swiper-item hide"},
+      {"left":"40%",  "width":"20%", "height":240, class:"swiper-item hide"},
+      {"left":"40%",  "width":"20%", "height":240, class:"swiper-item hide"},
+    ]
+    // 容器
+    var wrap = document.getElementById('wrap');
+    var cont = wrap.firstElementChild || wrap.firstChild;
     var btnArr=wrap.getElementsByTagName('a');
     var falg=true;
     var speed=7000;
+    // 鼠标悬浮停止轮播
     wrap.onmouseover=function(){
-        for (var i=0;i<btnArr.length;i++) {
-            btnArr[i].style.display='block';
-        }
-        clearInterval(wrap.timer);
+      console.log('停止轮播!')
+      clearInterval(wrap.timer);
     }
-    wrap.onmouseout=function(){
-        for (var i=0;i<btnArr.length;i++) {
-            btnArr[i].style.display='none';
-        }
-        wrap.timer=setInterval(function(){
-             move(true);
-         },speed);
-    }
-    var liArr=cont.children;
+    // 鼠标移出开始轮播
+    // wrap.onmouseout=function(){
+    //   wrap.timer = setInterval(function(){
+    //     move(true)
+    //   },speed);
+    // }
+    var liArr = cont.children;
+    // 排列
     move();
-    wrap.timer=setInterval(function(){
-        move(true);
-    },speed);
+    // 开启自动轮播
+    wrap.timer = setInterval(function(){
+      move(true);
+    }, speed);
+    // 前进按钮点击
     btnArr[1].onclick=function(){
-        if (falg) {
-            move(true);
-        }
+      if (falg) {
+        move(true)
+      }
     }
+    // 后退按钮点击
     btnArr[0].onclick=function(){
-        if (falg) {
-            move(false);
-        }
+      if (falg) {
+        move(false)
+      }
     }
-    function move(bool){
-        if(bool!=undefined){
-            if(bool){
-                size.unshift(size.pop());
-            }else {
-                size.push(size.shift());
-            }
+    function move(bool) {
+      if(bool != undefined){
+        if (bool) {
+          size.unshift(size.pop());
+        } else {
+          size.push(size.shift());
         }
-        for (var i=0;i<liArr.length;i++) {
-            animate(liArr[i],size[i],function(){
-                falg=true;
-            });
-        }
+      }
+      for (var i=0; i < liArr.length; i++) {
+        animate(liArr[i], size[i], function() {
+          falg = true
+        })
+      }
     }
 }
--- js/tools.js
+++ js/tools.js
@@ -3,41 +3,30 @@ function getStyle(obj,attr){
 	return obj.currentStyle ? obj.currentStyle[attr] : window.getComputedStyle(obj,null)[attr];
 }
 
-function animate(obj,json,fn){
-	clearInterval(obj.timer);
-	obj.timer=setInterval(function(){
-		var bool=true;
-		for(var k in json){
-			var leader;
-			if (k=='opacity') {
-				if (getStyle(obj,k)==undefined) {
-					leader=100;
-				}else {
-					leader=parseInt(getStyle(obj,k)*100);
-				}
-			}else {
-				leader=parseInt(getStyle(obj,k)) || 0;
-			}
-			var step=(json[k]-leader)/10;
-			step=step>0?Math.ceil(step):Math.floor(step);
-			leader=leader+step;
-			if(k=='zIndex'){
-				obj.style[k]=json[k];
-			}else if(k=='opacity'){
-				obj.style[k]=leader/100;
-				obj.style.filter='alpha(opacity='+leader+')';
-			}else {
-				obj.style[k]=leader+'px';
-			}
-			if(json[k]!=leader){
-				bool=false;
+function animate(obj, styleList, fn) {
+	// console.log(styleList)
+	setTimeout(() => {
+		for(var styleItem in styleList) {
+			var value = styleList[styleItem]
+			if(styleItem == 'zIndex'){
+				obj.style[styleItem] = styleList[styleItem];
+			} else if(styleItem=='opacity'){
+				obj.style[styleItem]=value/100;
+				obj.style.filter='alpha(opacity='+value+')';
+			} else if (styleItem == 'left') {
+				obj.style[styleItem] = styleList[styleItem];
+			} else if (styleItem == 'right') {
+				obj.style[styleItem] = styleList[styleItem];
+			} else if (styleItem == 'width') {
+				obj.style[styleItem] = styleList[styleItem];
+			} else if (styleItem != 'class') {
+				// console.log(value)
+				obj.style[styleItem] = value + 'px';
 			}
 		}
-		if(bool){
-			clearInterval(obj.timer);
-			if (fn) {
-				fn();
-			}
+		obj.setAttribute("class", styleList.class)
+		if (fn) {
+			fn();
 		}
-	},10);
+	}, 10)
 }
